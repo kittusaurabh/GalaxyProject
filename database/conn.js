d275d0ecfe203a2bce269bf8d84c5f604a17d744
kittusaurabh <itssaurabhbhushan@gmail.com>
2022-05-20 23:34:00 +0530

    abc

--- database/conn.js
+++ database/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/galaxyData', {
+mongoose.connect('mongodb+srv://saurabh:saurabh@cluster0.ka9ng.mongodb.net/?retryWrites=true&w=majority', {
 
 }).then(() => {
     console.log("Connection is set.")
